da3e23cfb93279870c54702243a39d30cc39a047
fdmk <61625353+fdmk@users.noreply.github.com>
2020-03-21 21:04:52 +0700

    Update soal2.c

--- Soal2/soal2.c
+++ Soal2/soal2.c
@@ -1,154 +1,172 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <wait.h>
-#include <time.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdio.h>//intuk int dan sebagainya
+#include <wait.h>// untuk mengambil nilai waktu
+#include <time.h>//untuk melakukan wait process
+#include <string.h>//untuk string
+#include <stdbool.h>//untuk perulangan
+#include <curl/curl.h>//untuk download
+#include <stdlib.h>// untuk EXIT_FAILURE & EXIT_SUCCESS
 #include <errno.h>
-#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>//untuk fungsi umask dan mkdir
+#include <fcntl.h>
 #include <syslog.h>
-#include <stdbool.h>
-#include <curl/curl.h> //untuk download
+#include <unistd.h>// untuk STDIN_FILENO || STDOUT_FILENO || STDERR_FILENO
 
 int main(){
-    //====>> membuat daemon (untuk membuat folder setiap 30 dektik)
-    pid_t pid,sid; //varibel untuk menyimpan PID
-    pid=fork(); //meyimpan pid dari child process
+     //====>> membuat daemon
+            pid_t pid;//variabel untuk menyimpan PID
+            pid_t sid;//varibel unuk menyimpan SID
+            //menyiapkan variabel-variabel sebagai proses untuk pembuatan 
+            pid_t point_b;
+            pid_t point_a;
+            pid_t sudo;
 
-    //jika gagal
-    if(pid<0){
-        exit(EXIT_FAILURE);
-    }
-    //jika berhasil
-    if(pid>0){
-        exit(EXIT_SUCCESS);
-    }
+            pid=fork(); //menyimpan PID dari child
 
-    umask(0); //mendapatkan akses full dari file daemon yang dibuat
+            //jika gagal
+            if(pid<0){
+                exit(EXIT_FAILURE);
+            }
 
-    sid=setsid(); //suapaya tidak menjadi orphan process
-    
-    //membuat process menjadi independen
-    if(sid<0){
-        exit(EXIT_FAILURE);
-    }
-    if((chdir("/home/basuki/code_test/tugmod2_sisop/")) < 0){
-        exit(EXIT_FAILURE);
-    }
+            //jika berhasil
+            if(pid>0){
+                exit(EXIT_SUCCESS);
+            }
+            
+            umask(0);//mendapatkan akses penuh dari process yang dibuat
 
-    close(STDIN_FILENO);
-    close(STDOUT_FILENO);
-    close(STDERR_FILENO);
-    
+            sid=setsid();//mencegah process child menjadi process orphan
+
+            //menjadikan process independen
+            if(sid<0){
+                exit(EXIT_FAILURE);
+            }
+            if(chdir("/home/basuki/code_test/tugmod2_sisop/")<0){
+                exit(EXIT_FAILURE);
+            }
+
+            close(STDIN_FILENO);//menutup akses input keyboard kedalam process
+            close(STDOUT_FILENO);//menutup akses output untuk menapilkan process ke layar
+            close(STDERR_FILENO);//menutup akses peringatan error kepengguna
+
     //mulai program daemon
     while(true){
-        //--->> bagian A, membuat folder
 
-        //membuat char untuk menyimpan nama folder dengan format waktu
-        char Time_dir[50];//untuk nama directory
-        char Time_img[50];//untuk nama image
-        
-        ///waktu
-        //mengambil waktu sekarang
-        time_t t=time(NULL);
-        struct tm tm=*localtime(&t);
-
-        //memasukkan waktu kedalam char Time_dir
-        sprintf(Time_dir,"%d-%02d-%02d_%02d:%02d:%02d",tm.tm_year+1900,tm.tm_mon+1,tm.tm_mday,tm.tm_hour,tm.tm_min,tm.tm_sec);
-                
-        //char untuk directory penyimpanan directory baru
-        char dir[50]={"/home/basuki/code_test/tugmod2_sisop/"};
-        
-        //untuk mkdir
-        int check;
-        
-        //make directory
-        check= mkdir(strcat(dir,Time_dir),0777);
-    
-        //change directory
-        chdir(Time_dir);
-            //perror("chdir() to directory failed");
-
-        //--->> bagian B, download
-        CURL *curl;
-        int result;
-
-        //membuat variabel epoch unix
-        long int now_epoch;
-        int size_phot;
-
-        //membuat file untuk menyimpan hasil download
-        FILE *fp;
-
-        char *url="https://i.picsum.photos/id/";
-        char img[50];//sebagai penyimpan sting link
-
-        //download
-        curl=curl_easy_init();
-        for(int foto_id=1;foto_id<=20;foto_id++)
+        sudo=fork();
+        wait(NULL);
+        if(sudo==0)
         {
-            ///waktu
-            //mengambil waktu sekarang
+            char Time_dir[50];//untuk nama directory
+            char Time_img[50];//untuk nama image
+
+            long int now_epoch;//variabel penyimpan nilai epoch time
+            int size_phot;//variabel penyimpan ukuran foto berdasarkan epoch time
+
+            //sebagai penyimpan workdirectory
+            char dir[50]={"/home/basuki/code_test/tugmod2_sisop/"};
+        
+            char *url="https://i.picsum.photos/id/";
+            char link[50];//penyimpan link akhir
+
+            //mengambil waktu
             time_t t=time(NULL);
             struct tm tm=*localtime(&t);
 
-            //memasukkan waktu kedalam char Time_img sebagai nama image
-            sprintf(Time_img,"%d-%02d-%02d_%02d:%02d:%02d",tm.tm_year+1900,tm.tm_mon+1,tm.tm_mday,tm.tm_hour,tm.tm_min,tm.tm_sec);
-            
-            //time epoch
-            time_t epoch_now=time(NULL);
+            //memasukkan timestamp kedalam Time_dir
+            sprintf(Time_dir,"%d-%02d-%02d_%02d:%02d:%02d",tm.tm_year+1900,tm.tm_mon+1,tm.tm_mday,tm.tm_hour,tm.tm_min,tm.tm_sec);
+        
+            ///===>>> PERSIAPAN DOWNLOAD
+            //persiapan untuk membuat file hasil download image
+            FILE *fp;
+            //mengambil dari library lcurl.h
+            CURL *curl;
+            int result;
 
-            //mengubah ukuran berdasarkan epoch time
-            now_epoch=epoch_now;
-            size_phot=(now_epoch%1000)+100;
+            //pembuatan point a
+            point_a=fork();
+            wait(NULL);//menyelesaikan dulu proses child
 
-            //memberikan nama link
-            sprintf(img,"%s%d/%d/%d.jpg",url,foto_id,size_phot,size_phot);
+            //pembuatan point a
+            if(point_a == 0){
+                mkdir(strcat(dir,Time_dir),0777);
+            }
 
-            //memberikan nama image dengan timestamp
-            fp=fopen(strcat(Time_img,".jpg"), "wb");
+            else
+            {        
+                //pembuatan point b
+                point_b=fork();
+                wait(NULL);
 
-            printf("%s\n",img);
-            
-            //proses download
-            curl_easy_setopt(curl, CURLOPT_URL,img);
-            curl_easy_setopt(curl,CURLOPT_WRITEDATA, fp);
-            curl_easy_setopt(curl,CURLOPT_FAILONERROR, 1L);
-
-            result= curl_easy_perform(curl);
-                if(result==CURLE_OK)
+                if(point_b<0)
+                {exit(EXIT_FAILURE);}
+                
+                if(point_b==0);
                 {
-                    printf("Download Successful!\n");
+                    //change directory
+                    chdir(Time_dir);
+                    
+                    //download
+                    curl=curl_easy_init();
+                    for(int id_foto=1;id_foto<=20;id_foto++)
+                    {
+                        //mengambil dari time untuk nama image
+                        time_t T=time(NULL);
+                        struct tm TM=*localtime(&T);
+
+                        //memasukkan time kedalam variabel Time_img
+                        sprintf(Time_img,"%d-%02d-%02d_%02d:%02d:%02d",TM.tm_year,TM.tm_mon,TM.tm_mday,TM.tm_hour,TM.tm_min,TM.tm_sec);
+
+                        //time epoch unix
+                        time_t epoch_now=time(NULL);
+
+                        //mengubah ukuran berdasarkan epoch time
+                        now_epoch=epoch_now;
+                        size_phot=(now_epoch%1000)+100;
+                        
+                        //melengkapkan link yang diberikan
+                        sprintf(link,"%s%d/%d/%d.jpg",url,id_foto,size_phot,size_phot);
+
+                        //membuat file hasil download sekagus dengan memberikan nama image
+                        fp=fopen(strcat(Time_img,".jpg"), "wb");
+
+                        //proses download
+                        curl_easy_setopt(curl,CURLOPT_URL,link);
+                        curl_easy_setopt(curl,CURLOPT_WRITEDATA, fp);
+                        curl_easy_setopt(curl,CURLOPT_FAILONERROR, 1L);
+
+                        curl_easy_perform(curl);
+                        fclose(fp);
+                        sleep(5);
+                    }
+                    curl_easy_cleanup(curl);
+
+                    //kembali ke directory sebelumnya
+                    chdir("..");
+
+                    ///Bagian C
+                    //untuk zip
+                    pid_t point_c;// untuk penyelesaian pont c;
+                    point_c=fork();
+
+                    //membuat zip
+                    wait(NULL);
+                    if(point_c==0)
+                    {
+                        //membuat zip dengan format [nama_zip].zip [nama_file(yang akan dicompress)]
+                        char *compress[]={"zip","-r",Time_dir,Time_dir,NULL};
+                            execv("/bin/zip", compress);
+                    }
+                    else
+                    {
+                        remove(Time_dir);//delete directory
+                    }
                 }
-                else
-                {
-                    printf("Error: %s\n",curl_easy_strerror(result));
-                }
-            fclose(fp);
-            sleep(5);
-        }
-        curl_easy_cleanup(curl);
-
-        //kembali ke directory sebelumya
-        chdir("..");
-
-        //untuk zip
-        pid_t zip;
-        zip=fork();
-
-        //membuat zip
-        wait(NULL);
-        if(zip==0)
-        {        
-            char *zip[]={"zip","-r",Time_dir,Time_dir,NULL};
-                execv("/bin/zip", zip);
+            }
         }
         
-        remove(Time_dir);//delete directory
-
-        sleep(30);
+        else
+        {
+            sleep(30);
+        }
     }
 }
